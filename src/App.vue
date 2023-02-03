fda1731034d1e6fec2599dc55b855040438d517f
html1024 <33651504+html1024@users.noreply.github.com>
2023-02-03 14:17:40 +0800

    2 files modified, 1 added

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
 </script>
 
 <template>
-  <div>
+  <div v-if="false">
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
